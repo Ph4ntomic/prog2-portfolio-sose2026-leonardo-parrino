36d8fe6cb115f8f27108fb9b708e63227cd8ddcd
Leonardo Parrino <Leonardorosarioparrino@gmail.com>
2026-04-27 18:33:51 +0200

    build: add Spotless demo

---
+++ B01/SpotlessDemonstrieren.java
@@ -0,0 +1,14 @@
+public class SpotlessDemonstrieren {
+    public static void main(String[] args) {
+        IO.println(System.getProperty("java.version"));
+}
+}
+
+/*
+}
+}
+absichtlich "hässlich" untereinander um Spotless zu demostrieren.
+
+ausführen zum fixen:
+.\gradlew.bat spotlessApply
+ */
